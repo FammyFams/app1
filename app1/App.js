a3c1d7b317334855b7417a8c76aa27a598d69271
Matthew <matthewzhenghi@gmail.com>
2020-12-17 19:28:30 -0800

    testing

--- app1/App.js
+++ app1/App.js
@@ -17,7 +17,7 @@ export default function App() {
         style={{
           backgroundColor: 'dodgeblue',
           width: '100%',
-          height: '30%',
+          height: '40%',
         }}
       ></View>
     </SafeAreaView>
