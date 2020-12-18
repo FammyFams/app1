fb11343e865853ffaf2105494f8e022619977063
Matthew <matthewzhenghi@gmail.com>
2020-12-17 19:22:04 -0800

    first commit

---
+++ app1/App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+  StyleSheet,
+  Platform,
+  SafeAreaView,
+  Dimensions,
+  View,
+} from 'react-native';
+
+export default function App() {
+  console.log(Dimensions.get('screen'));
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: 'dodgeblue',
+          width: '100%',
+          height: '30%',
+        }}
+      ></View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
